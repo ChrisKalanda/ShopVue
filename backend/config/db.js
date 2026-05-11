8c002f9360141c59d743cccd0f5ea58b939eec77
Chris Kalanda <chriskalanda@teccart.qc.ca>
2026-05-10 20:53:24 -0400

    Add DB_PORT to pool config

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ console.log("PASSWORD:", process.env.DB_PASSWORD);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
